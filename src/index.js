e77bb0b019081179c27f0b02a5cc0b9144e70fa5
theresialindholm <80003084+theresialindholm@users.noreply.github.com>
2021-04-14 21:27:44 +0200

    Started CSS of forecast and display of forecast data using API

--- src/index.js
+++ src/index.js
@@ -21,26 +21,41 @@ form.addEventListener("submit", updateCity);
 let searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener("click", updateCity);
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function(day){
+  
+  forecast.forEach(function(forecastDay, index){
+if (index <6) {
+
     forecastHTML = forecastHTML + `
 
     <div class="col-2">
       <div class="forecast-day" >
-        ${day}
-        <br />
-        <p class="weekly-weather-icon-">
-          <i class="fas fa-cloud-sun"></i>
-        </p>
-        10°c
+        ${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+                     alt=""
+                     id="icon">
+        <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">
+        ${Math.round(forecastDay.temp.max)}° </span>
+        <span class="weather-forecast-temperature-min">
+        ${Math.round(forecastDay.temp.min)}° </span>
       </div>
     </div>
   `;
+}
   })
 
         forecastHTML = forecastHTML+ `</div>`;
